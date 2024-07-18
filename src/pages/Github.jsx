e10cabe9176c3404b30942480e03704a9638ec3f
bkjonathan <kyawhtetsan.jar1990@gmail.com>
2024-07-18 17:28:44 +0700

    Optimize Github page

--- src/pages/Github.jsx
+++ src/pages/Github.jsx
@@ -6,7 +6,7 @@ const Github = () => {
     <div className="text-center m-5 bg-gray-500 text-white p-3 text-xl">
       <div>Github followers: {Number(user.followers).toLocaleString()}</div>
       <div className="text-center">
-        <img src={user.avatar_url} alt="git picutre" className="text-center" />
+        <img src={user.avatar_url} alt="git picutre" className="text-center" loading="lazy" />
       </div>
     </div>
   );
